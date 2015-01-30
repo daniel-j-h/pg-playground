75b592f6b4711ec01bd659c6a044a7ac8f6adb6b
Daniel Hofmann <daniel@trvx.org>
2015-01-30 15:49:49 +0100

    Index, query

--- src/pg_playground/json.clj
+++ src/pg_playground/json.clj
@@ -20,7 +20,18 @@
   ((juxt drop-table create-table) table))
 
 
-(defn docs->table [table docs]
+(defn insert-docs [table docs]
   (jdbc/with-db-connection [con db]
     (doseq [doc docs]
       (jdbc/insert! con table {:doc doc}))))
+
+; (doseq [x (range 2) y (range 2)] (j/insert-docs :playground [{:x x :y y}]))
+
+
+(defn create-index [table]
+  (jdbc/execute! db ["create index on playground(cast(doc->>'x' as int));"]))
+
+
+(defn query-docs [table]  ; table can't be parameterized in prepared stmt
+  (jdbc/with-db-connection [con db]
+    (jdbc/query con ["select id, doc from playground;"])))
